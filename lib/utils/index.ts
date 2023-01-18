53e6b1b11fb74e179fc9aae5a9150a50f7bed068
Max Jonas <61575471+maxjonas2@users.noreply.github.com>
2023-01-18 14:10:52 +0000

    Fix in import statement

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,11 +1,11 @@
 import artists from "../data/artists.json";
-import tracks, { Track } from "../data/tracks";
+import tracks, { ITrack } from "../data/tracks";
 
 export function getArtists() {
   return new Promise((resolve) => setTimeout(() => resolve(artists), 0));
 }
 
-export function getTracksByArtistId(id: number): Promise<Track[]> {
+export function getTracksByArtistId(id: number): Promise<ITrack[]> {
   const tracklist = tracks[id].recent;
   return new Promise((resolve) => setTimeout(() => resolve(tracklist), 0));
 }
