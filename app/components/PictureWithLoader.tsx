0ac440d7ef31045e050c60ea4ffa1ac00c81dbdc
Max Jonas <61575471+maxjonas2@users.noreply.github.com>
2023-01-25 15:22:14 -0300

    Update PictureWithLoader.tsx

--- app/components/PictureWithLoader.tsx
+++ app/components/PictureWithLoader.tsx
@@ -14,7 +14,7 @@ const PictureWithLoader = ({ imageSrc }: { imageSrc: string }) => {
   return (
     <picture className="block absolute inset-0">
       <Image
-        src={"https://spot-clone-next.vercel.app/assets/" + imageSrc}
+        src={"https://spot-clone-next.vercel.app/assets/tracks/recent/" + imageSrc}
         className="w-full h-full object-cover object-center rounded-[.5rem]"
         fill={true}
         alt="Something for now"
